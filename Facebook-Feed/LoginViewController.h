54ee9ea5136e062fc6aa2b670e0a26cc39e2c2f6
jairoavalos <jairoavalos@Jairos-Gametime-MacBook-Air.local>
2014-06-11 21:02:21 -0700

    Adding first view
    Login

---
+++ Facebook-Feed/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Facebook-Feed
+//
+//  Created by Jairo Avalos on 6/11/14.
+//  Copyright (c) 2014 Jairo Avalos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Facebook-Feed/LoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.m
+//  Facebook-Feed
+//
+//  Created by Jairo Avalos on 6/11/14.
+//  Copyright (c) 2014 Jairo Avalos. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Setting app status bar to white
+         [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
